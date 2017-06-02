77af807626ccc7db48a154939f41155960b4fd1d
Tyler Long <tyler4long@gmail.com>
2017-06-02 17:20:05 +0800

    First working version

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
-
 const fs = require('fs')
 const chalk = require('chalk')
+const child_process = require('child_process')
 
 const error = (message) => {
   console.log(chalk.red('Error: ') + message)
+}
+const info = (message) => {
+  console.log(chalk.blue('Info: ') + message)
 }
 
 if(!fs.existsSync('./package.json')) {
@@ -12,5 +15,13 @@ if(!fs.existsSync('./package.json')) {
   return;
 }
 
-const s = fs.readFileSync('./package.json', 'utf-8')
-console.log(s)
+const packageJson = require('./package.json')
+for(let element of ['dependencies', 'devDependencies', 'peerDependencies']) {
+  if(packageJson[element]) {
+    const packages = Object.keys(packageJson[element])
+    for(let package of packages) {
+      info(`yarn upgrade ${package}`)
+      child_process.execSync(`yarn upgrade ${package}`, { stdio: [] })
+    }
+  }
+}
