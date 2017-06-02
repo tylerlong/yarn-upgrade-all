a12c7ab7aa2b75cc5772aa5d19406e876466f7fe
Tyler Long <tyler4long@gmail.com>
2017-06-02 18:14:46 +0800

    Refactor code

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 const fs = require('fs')
+const path = require('path')
+const childProcess = require('child_process')
 const chalk = require('chalk')
-const child_process = require('child_process')
 
 const error = (message) => {
   console.log(chalk.red('Error: ') + message)
@@ -10,18 +11,19 @@ const info = (message) => {
   console.log(chalk.blue('Info: ') + message)
 }
 
-if(!fs.existsSync('./package.json')) {
-  error('Cannot find package.json file in the current directory');
-  return;
+const packagePath = path.resolve(process.cwd(), 'package.json')
+if (!fs.existsSync(packagePath)) {
+  error('Cannot find package.json file in the current directory')
+  process.exit()
 }
 
-const packageJson = require('./package.json')
-for(let element of ['dependencies', 'devDependencies', 'peerDependencies']) {
-  if(packageJson[element]) {
+const packageJson = require(packagePath)
+for (let element of ['dependencies', 'devDependencies', 'peerDependencies']) {
+  if (packageJson[element]) {
     const packages = Object.keys(packageJson[element])
-    for(let package of packages) {
-      info(`yarn upgrade ${package}`)
-      child_process.execSync(`yarn upgrade ${package}`, { stdio: [] })
+    for (let pkg of packages) {
+      info(`yarn upgrade ${pkg}`)
+      childProcess.execSync(`yarn upgrade ${pkg}`, { stdio: [] })
     }
   }
 }
