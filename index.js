23ce7701129378d41e052fee36e53453df17a6f7
Tyler Long <tyler4long@gmail.com>
2017-06-02 18:43:19 +0800

    Async instead of sync

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const path = require('path')
 const childProcess = require('child_process')
 const chalk = require('chalk')
 
-const error = (message) => {
+const logError = (message) => {
   console.log(chalk.red('Error: ') + message)
 }
-const info = (message) => {
-  console.log(chalk.blue('Info: ') + message)
+const logDone = (message) => {
+  console.log(chalk.green('Done: ') + message)
 }
 
 const packagePath = path.resolve(process.cwd(), 'package.json')
 if (!fs.existsSync(packagePath)) {
-  error('Cannot find package.json file in the current directory')
+  logError('Cannot find package.json file in the current directory')
   process.exit()
 }
 
@@ -22,8 +22,13 @@ for (let element of ['dependencies', 'devDependencies', 'peerDependencies']) {
   if (packageJson[element]) {
     const packages = Object.keys(packageJson[element])
     for (let pkg of packages) {
-      info(`yarn upgrade ${pkg}`)
-      childProcess.execSync(`yarn upgrade ${pkg}`, { stdio: [] })
+      childProcess.exec(`yarn upgrade ${pkg}`, { stdio: [] }, (error, stdout, stderr) => {
+        if (error) {
+          logError(`yarn upgrade ${pkg}: ${error}`)
+        } else {
+          logDone(`yarn upgrade ${pkg}`)
+        }
+      })
     }
   }
 }
