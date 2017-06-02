2bbf9459920b0ee80f3e3a88904ae2bc78411240
Tyler Long <tyler4long@gmail.com>
2017-06-02 16:41:56 +0800

    Read package.json

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('hello world! I am yarn-upgrade-all!')
+#!/usr/bin/env node
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+const error = (message) => {
+  console.log(chalk.red('Error: ') + message)
+}
+
+if(!fs.existsSync('./package.json')) {
+  error('Cannot find package.json file in the current directory');
+  return;
+}
+
+const s = fs.readFileSync('./package.json', 'utf-8')
+console.log(s)
