7918a1b441136ee2f2cdf8c8ed9b5acfe97d98ff
Tyler Long <tyler4long@gmail.com>
2017-09-15 19:35:13 +0800

    Do it the hard way

--- index.js
+++ index.js
@@ -5,28 +5,39 @@ const childProcess = require('child_process')
 const chalk = require('chalk')
 
 const logError = (message) => {
-  console.log(chalk.red('Error: ') + message)
+  console.log(chalk.red('[Error]: ' + message))
 }
-const logDone = (message) => {
-  console.log(chalk.green('Done: ') + message)
+const logInfo = (message) => {
+  console.log(chalk.blue('[Start]: ' + message))
+}
+const logSuccess = (message) => {
+  console.log(chalk.green('[Done]: ' + message))
 }
 
 const packagePath = path.resolve(process.cwd(), 'package.json')
 if (!fs.existsSync(packagePath)) {
   logError('Cannot find package.json file in the current directory')
-  process.exit()
+  process.exit(1)
 }
 
 const packageJson = require(packagePath)
+const options = {
+  dependencies: '',
+  devDependencies: ' --dev',
+  peerDependencies: ' --peer'
+}
 for (let element of ['dependencies', 'devDependencies', 'peerDependencies']) {
   if (packageJson[element]) {
+    const option = options[element]
     const packages = Object.keys(packageJson[element])
     for (let pkg of packages) {
+      const command = `yarn remove ${pkg} && yarn add${option} ${pkg}`
       try {
-        childProcess.execSync(`yarn upgrade ${pkg}`, { stdio: [] })
-        logDone(`yarn upgrade ${pkg}`)
+        logInfo(command)
+        childProcess.execSync(command, { stdio: [] })
+        logSuccess(command)
       } catch (e) {
-        logError(`yarn upgrade ${pkg} - ${e}`)
+        logError(`${command} - ${e}`)
       }
     }
   }
