ed053ce1f2c4669aa03727152a8674864ad7dc9a
Tyler Liu <tyler4long@gmail.com>
2026-04-22 10:55:42 -0700

    Use biome to format and lint

--- src/index.ts
+++ src/index.ts
@@ -15,54 +15,54 @@ const inputs = new Set(process.argv);
 let packagePath: PathLike;
 let global = "";
 if (inputs.has("-g") || inputs.has("--global")) {
-  global = "global";
-  packagePath = resolve(
-    process.env[process.platform === "win32" ? "USERPROFILE" : "HOME"]!,
-    ".config",
-    "yarn",
-    "global",
-    "package.json",
-  );
+	global = "global";
+	packagePath = resolve(
+		process.env[process.platform === "win32" ? "USERPROFILE" : "HOME"]!,
+		".config",
+		"yarn",
+		"global",
+		"package.json",
+	);
 } else {
-  packagePath = resolve(process.cwd(), "package.json");
+	packagePath = resolve(process.cwd(), "package.json");
 }
 if (!existsSync(packagePath)) {
-  error.log(`Cannot find ${packagePath}`);
-  process.exit(1);
+	error.log(`Cannot find ${packagePath}`);
+	process.exit(1);
 }
 
 const packageJson = require(packagePath);
 let ignorePkgs = new Set();
 if (packageJson["yarn-upgrade-all"] && packageJson["yarn-upgrade-all"].ignore) {
-  ignorePkgs = new Set(packageJson["yarn-upgrade-all"].ignore);
+	ignorePkgs = new Set(packageJson["yarn-upgrade-all"].ignore);
 }
 
 const depTypes: { [key: string]: string } = {
-  dependencies: "",
-  devDependencies: "--dev",
-  peerDependencies: "--peer",
+	dependencies: "",
+	devDependencies: "--dev",
+	peerDependencies: "--peer",
 };
-const params = Array.from(inputs).filter((arg) =>
-  arg.startsWith("-") && arg !== "-g" && arg !== "--global"
+const params = Array.from(inputs).filter(
+	(arg) => arg.startsWith("-") && arg !== "-g" && arg !== "--global",
 );
 Object.keys(depTypes).forEach((depType) => {
-  if (!packageJson[depType]) {
-    return;
-  }
-  const deps = Object.keys(packageJson[depType])
-    .filter((dep) => !ignorePkgs.has(dep))
-    .filter((dep) => !packageJson[depType][dep].startsWith(".")) // relative path
-    .filter((dep) => !packageJson[depType][dep].startsWith("file:")); // file path
-  if (deps.length === 0) {
-    return;
-  }
-  const argv = ["yarn", global, "add", ...deps, depTypes[depType], ...params];
-  const command = argv.filter((c) => c !== "").join(" ");
-  try {
-    info.log(command);
-    execSync(command, { stdio: [] });
-    success.log(command);
-  } catch (e) {
-    error.log(`${command} - ${e}`);
-  }
+	if (!packageJson[depType]) {
+		return;
+	}
+	const deps = Object.keys(packageJson[depType])
+		.filter((dep) => !ignorePkgs.has(dep))
+		.filter((dep) => !packageJson[depType][dep].startsWith(".")) // relative path
+		.filter((dep) => !packageJson[depType][dep].startsWith("file:")); // file path
+	if (deps.length === 0) {
+		return;
+	}
+	const argv = ["yarn", global, "add", ...deps, depTypes[depType], ...params];
+	const command = argv.filter((c) => c !== "").join(" ");
+	try {
+		info.log(command);
+		execSync(command, { stdio: [] });
+		success.log(command);
+	} catch (e) {
+		error.log(`${command} - ${e}`);
+	}
 });
